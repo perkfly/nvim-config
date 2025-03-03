3ee12a582f4cf77e112261f30ee9f83902012feb
Yifei Kong <kong@yifei.me>
2025-03-03 15:27:24 +0800

    Add llama.vim

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -11,6 +11,7 @@ require("lazy").setup({
       update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
     },
   },
+  {"ggml-org/llama.vim"},
   { import = "community" },
   { import = "plugins" },
 } --[[@as LazySpec]], {
